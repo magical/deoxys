d2050b86d111b3807e9330f6a18a6c8d07bea4f7
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2016-10-05 20:40:45 -0700

    Speed asm up 2.6× by changing a single instruction
    Turns out the LOOP instruction is really slow. Replacing
    LOOP with an explicit SUBQ / JNZ sequence yields a 2.6×
    performance increase.
    
    I also tried reordering some instructions, but it didn't really have
    any observable effect.
    
    benchmark                old ns/op     new ns/op     delta
    BenchmarkExpandKey-4     887           910           +2.59%
    BenchmarkDeoxys-4        42.4          16.3          -61.56%
    
    benchmark             old MB/s     new MB/s     speedup
    BenchmarkDeoxys-4     377.39       980.79       2.60x

--- deoxys_amd64.s
+++ deoxys_amd64.s
@@ -1,10 +1,10 @@
 #include "textflag.h"
 
-DATA h<>+0(SB)/8, $0x000f0a050c0b0601
-DATA h<>+8(SB)/8, $0x0807020d04030e09
-GLOBL h<>(SB), (RODATA|NOPTR), $16
+DATA permutation<>+0(SB)/8, $0x000f0a050c0b0601
+DATA permutation<>+8(SB)/8, $0x0807020d04030e09
+GLOBL permutation<>(SB), (RODATA|NOPTR), $16
 
-TEXT ·encryptBlockAsm(SB), $0-96
+TEXT ·encryptBlockAsm(SB), NOSPLIT, $0-96
     // TODO check bounds of in, out, and tweak?
 
     MOVQ subkey_len+8(FP), CX
@@ -20,13 +20,13 @@ TEXT ·encryptBlockAsm(SB), $0-96
     MOVOU (AX), X2
 
     // Load the tweak permutation
-    MOVOU h<>(SB), X4
+    MOVOU permutation<>(SB), X4
 
     // XOR the first subtweakey into the message
     MOVOU (BX), X1
     PXOR X2, X1
-    PXOR X1, X0
     ADDQ $16, BX
+    PXOR X1, X0
 
 loop:
     // Permute the tweak
@@ -40,8 +40,8 @@ loop:
     // Encrypt
     AESENC X1, X0
 
-    LOOP loop
-end:
+    SUBQ $1, CX
+    JNZ loop
 
     // Store the result
     MOVQ out_base+72(FP), BX
